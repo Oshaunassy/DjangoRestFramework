2a2b966b06be54a29f674419592922035a6d239e
Oshaunassy <144370985+Oshaunassy@users.noreply.github.com>
2024-03-18 22:19:03 +0600

    first lesson

---
+++ product/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Product, Review
+
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = 'id title price review'.split()
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'product, stars'.split()
